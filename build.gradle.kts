6de3553204bce67b6ce596fc6015fbc2d89cd80c
Aiko Vandeputte <aiko@aikovdp.me>
2023-04-19 14:27:23 +0200

    Upgrade Gradle to 8.1 and move to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    java
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "com.rokucraft"
+version = "1.0.2"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+}
+
+dependencies {
+    compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
+    implementation("net.kyori:adventure-text-minimessage:4.1.0-SNAPSHOT") {
+        exclude(module = "adventure-api")
+    }
+    implementation("org.spongepowered:configurate-yaml:4.1.2")
+    implementation("cloud.commandframework:cloud-paper:1.8.3")
+    implementation("com.github.stefvanschie.inventoryframework:IF:0.10.9")
+}
+
+tasks {
+    shadowJar {
+        archiveClassifier.set("")
+        isEnableRelocation = true
+        relocationPrefix = "$group.$name.libs"
+    }
+    processResources {
+        val props = "version" to version
+        inputs.properties(props)
+        filteringCharset = "UTF-8"
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
